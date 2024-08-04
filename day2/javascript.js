86f33c24ccf5e2f336bf97de47872a29e6c2dc36
animesh <phb21013@tezu.ac.in>
2024-08-04 20:32:49 +0530

    Create javascript.js

---
+++ day2/javascript.js
@@ -0,0 +1,36 @@
+var a = "animesh"
+console.log(a);
+
+a = "kakoty"
+console.log(a);
+// we are changing values of variable a
+a = true
+console.log(a);
+// js won't complain here as it's Dynamically type 
+// Js is single threaded with asynchronous behaviour => not blocking 
+// js/java automatically manages mamory management ( using memeory) done by garbage collection => let me free up some memory
+
+
+/*
+    Create a variable for each of the following: 
+    your favorite color, your height in centimeters, 
+    and whether you like pizza. Use appropriate 
+    variable declarations (let, const, or var). Try logging it using console.log
+
+*/
+
+const name = "animesh"
+const age = 21
+let favColor = "blue"
+var height = 5.7
+var pizzaLike = false
+
+console.log(name);
+console.log(age);
+console.log(favColor);
+console.log(height);
+console.log(`like Pizza : `, pizzaLike);
+// use let & const 
+
+
+
