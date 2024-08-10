9a5ff299214328618a4dd644f235a898c23c018f
animesh <phb21013@tezu.ac.in>
2024-08-10 20:11:08 +0530

    di

---
+++ week1/cohort2.js
@@ -0,0 +1,32 @@
+/*
+  Write a function `findLargestElement` that takes an array of numbers and returns the largest element.
+  Example:
+  - Input: [3, 7, 2, 9, 1]
+  - Output: 9
+*/
+
+function findLargestElement(numbers) {
+    let largest = numbers[0]
+    for(let i = 1; i < numbers.length; i++){
+        if(numbers[i] > largest){
+            largest = numbers[i]
+        }
+        
+    }
+    return largest
+}
+
+
+console.log(findLargestElement([3, 7, 2, 9, 1]))
+
+
+/*
+  Implement a function `countVowels` that takes a string as an argument and returns the number of vowels in the string.
+  Note: Consider both uppercase and lowercase vowels ('a', 'e', 'i', 'o', 'u').
+
+  Once you've implemented the logic, test your code by running
+*/
+
+function countVowels(str) {
+    let vowels = [a, e, i, o, u, A]
+}
