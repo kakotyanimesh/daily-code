aef5b632eae60781d4d3e73206e92eb5d5ce663a
animesh <phb21013@tezu.ac.in>
2024-12-21 03:03:54 +0530

    im sleepy

--- framermotion/app/advanced/page.tsx
+++ framermotion/app/advanced/page.tsx
@@ -1,7 +1,22 @@
 'use client'
 
 import { motion } from "motion/react"
+import { useState } from "react"
 export default function AdvancedComponets() {
+    const [title, settitle] = useState('click')
+
+    const [bgColor, setBgColor] = useState(`{hsl(${Math.floor(Math.random() * 360)}, 70%, 97%)}`)
+    const [angle, setAngle] = useState(0)
+
+    const randomColor = () => {
+        setBgColor(`hsl(${Math.floor(Math.random() * 360)}, 80%, 50%)`)
+        setAngle(angle + 45)
+        
+        
+    }
+    const clickme = () => {
+        settitle('clicked')
+    }
     return (
         <div className="flex flex-col justify-center items-center text-center">
             <h1 className="bg-sky-600">scroll animation</h1>
@@ -13,6 +28,18 @@ export default function AdvancedComponets() {
                 <Card parentColor="#e0e7ff" childColor="#c2410c"/>
                 <Card parentColor="#4a044e" childColor="#047857"/>
                 <Card parentColor="#e9d5ff" childColor="#fb7185"/>
+            </div>
+
+            <motion.div whileHover={{backgroundColor : "#0891b2", boxShadow : "0px 0px 100px #881337"}} className="mt-44 bg-blue-300 flex flex-col gap-32 items-center justify-center w-72 rounded-xl h-64 ">
+                <motion.h1 whileHover={{rotate : 357}} className="text-4xl text-green-700 font-bold">Glow card</motion.h1>
+                <motion.button onTap={clickme}>{title}</motion.button>
+            </motion.div>
+
+            <div className="my-44 bg-blue-400 size-[600px] rounded-[100px] relative">
+                <motion.div className="size-[180px] bg-red-400 left-[45%] top-[20%] rounded-3xl absolute" onTap={randomColor} animate={{backgroundColor : bgColor, boxShadow : `0px 0px 100px ${bgColor}`, rotate : angle}}>
+
+                </motion.div>
+
             </div>
             
         </div>
