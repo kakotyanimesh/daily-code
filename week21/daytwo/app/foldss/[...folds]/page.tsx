8129fb5a722257a74a97619b5bac03fa247f475a
animesh <phb21013@tezu.ac.in>
2024-12-23 03:16:46 +0530

    drunk af

---
+++ week21/daytwo/app/foldss/[...folds]/page.tsx
@@ -0,0 +1,8 @@
+export default function Folsdd({params} : {params : {folds : string[]}}) {
+    const foldssss = params.folds
+
+    return <div>
+
+        {JSON.stringify(foldssss)}
+    </div>
+}
---
+++ week21/daytwo/app/foldss/page.tsx
@@ -0,0 +1,5 @@
+export default function foldess() {
+    return <div>
+        just used the catch eoutes in nextjs means [dot dot dot customname] here it is [...folds]
+    </div>
+}
