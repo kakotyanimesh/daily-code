df652c410e36653c7c4850dba8073be2248e43e2
animesh <phb21013@tezu.ac.in>
2025-01-01 22:43:40 +0530

    365daystrick

--- week22/my-turborepo/apps/wsbackend/src/index.ts
+++ week22/my-turborepo/apps/wsbackend/src/index.ts
@@ -1,52 +1,41 @@
 import { WebSocketServer, WebSocket } from "ws";
 
-const wss = new WebSocketServer({port : 8080})
-
 interface User {
-    soket : WebSocket,
+    soket :  WebSocket,
     roomId : string
 }
 
-let inmemorySokets : User[] = []
+let inmemmorySokets : User[] = []
+
+const wss = new WebSocketServer({port : 8080})
 
 wss.on('connection', (soket) => {
-    soket.on('message', (msg) => {
-        try {
-            const parsedMsg = JSON.parse(msg as unknown as string)
+    soket.on('message', (stringMsg) => {
+        const parsedMsg = JSON.parse(stringMsg as unknown as string)
 
-            if(parsedMsg.type === "join"){
-                inmemorySokets.push({
-                    soket,
-                    roomId : parsedMsg.payload.roomId
-                })
-                return
-            }
-
-            if(parsedMsg.type === "chat") {
-                const sender = inmemorySokets.find((x) => x.soket === soket)
-                if(!sender) {
-                    console.log('no sender found');
-                    return
-                }
-
-                const roomId = sender.roomId
-
-                const participants = inmemorySokets.filter((x) => x.roomId === roomId)
-                participants.forEach((x) => {
-                    if(x.soket !== soket){
-                        x.soket.send(parsedMsg.payload.message)
-                    }
-                })
-            }
-        } catch (error) {
-            console.log(`error : ${error}`);
-            
+        if(parsedMsg.type === 'join'){
+            inmemmorySokets.push({
+                soket,
+                roomId : parsedMsg.payload.roomId
+            })
+            return
         }
-    })
 
-    soket.on('close', () => {
-        inmemorySokets = inmemorySokets.filter((x) => x.soket !== soket)
-        console.log(`user disconnected total user : ${inmemorySokets.length}`);
-        
+        if(parsedMsg.type === "chat"){
+            const sender = inmemmorySokets.find((x) => x.soket === soket)
+            if(!sender){
+                console.log('no sender found');
+                return
+                
+            }
+
+            const roomId = sender.roomId
+            const participants = inmemmorySokets.filter((x) => x.roomId === roomId)
+            participants.forEach((x) => {
+                if(x.soket !== soket){
+                    x.soket.send(parsedMsg.payload.message)
+                }
+            } )
+        }
     })
 })
