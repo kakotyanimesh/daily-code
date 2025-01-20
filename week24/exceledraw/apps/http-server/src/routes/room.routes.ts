6771d0d3348fec639b4bf5153a53b0ac64000b6e
Turbobot <turbobot@vercel.com>
2025-01-20 18:01:47 +0530

    feat(create-turbo): apply official-starter transform

--- week24/exceledraw/apps/http-server/src/routes/room.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { joinRoom } from "../controller/room.controller";
-
-const roomRouter : Router = Router()
-
-roomRouter.post('/join', joinRoom)
-
-export default roomRouter
